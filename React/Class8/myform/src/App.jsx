3c79e6f48efa657f62ba749b5c879898c39d19e6
Shubham Kumar <shubham620409@gmail.com>
2025-12-20 20:41:40 +0530

    Form Completed

--- React/Class8/myform/src/App.jsx
+++ React/Class8/myform/src/App.jsx
@@ -9,19 +9,30 @@ const App = () => {
     streetAddress: "",
     city: "",
     state: "",
-    postalCode: ""
+    postalCode: "",
+    comments: false,
+    candidates: false,
+    offers: false,
+    pushNotifications: ""
   })
 
   function changeHandler(event) {
     const { name, value, checked, type } = event.target;
     setFormData((prev) => ({
-      ...prev, [name]: value
+      ...prev, [name]: type === "checkbox" ? checked : value
     }))
   }
 
+  function submitHandler(event) {
+    event.preventDefault();
+
+    console.log("Finally printing the value of Form Data: ");
+    console.log(formData);
+  }
+
   return (
-    <div className='flex flex-col items-center'>
-      <form>
+    <div className='flex flex-col items-center mt-2'>
+      <form onSubmit={submitHandler}>
 
         <br />
         <label htmlFor="firstName">First Name</label>
@@ -129,7 +140,96 @@ const App = () => {
           onChange={changeHandler}
           className='outline'
         />
+        <br /><br />
+        <fieldset>
+          <legend>By Email</legend>
 
+          <div className='flex'>
+            <input
+              id='comments'
+              name='comments'
+              type='checkbox'
+              checked={formData.comments}
+              onChange={changeHandler}
+            />
+            <div>
+              <label htmlFor="comments">Comments</label>
+              <p>Get notified when someones posts a comment on a posting.</p>
+            </div>
+          </div>
+
+          <div className='flex'>
+            <input
+              id='candidates'
+              name='candidates'
+              type='checkbox'
+              checked={formData.candidates}
+              onChange={changeHandler}
+            />
+            <div>
+              <label htmlFor="candidates">Candidates</label>
+              <p>Get notified when a candidate applies for a job.</p>
+            </div>
+          </div>
+
+          <div className='flex'>
+            <input
+              id='offers'
+              name='offers'
+              type='checkbox'
+              checked={formData.offers}
+              onChange={changeHandler}
+            />
+            <div>
+              <label htmlFor="offers">Offers</label>
+              <p>Get notified when a candidate accepts or rejects an offer.</p>
+            </div>
+          </div>
+
+        </fieldset>
+        <br />
+        <fieldset>
+          <legend>Push Notifications</legend>
+          <p>these are delivered via SMS to your mobile phone.</p>
+
+          <input
+            type='radio'
+            id='pushEverything'
+            name='pushNotifications'
+            value="Everything"
+            onChange={changeHandler}
+          />
+
+          <label htmlFor="pushEverything">Everything</label>
+
+          <br />
+
+          <input
+            type='radio'
+            id='pushEmail'
+            name='pushNotifications'
+            value="Same as email"
+            onChange={changeHandler}
+          />
+
+          <label htmlFor="pushEmail">Same as email</label>
+
+          <br />
+
+          <input
+            type='radio'
+            id='pushNothing'
+            name='pushNotifications'
+            value="No Push Notifications"
+            onChange={changeHandler}
+          />
+
+          <label htmlFor="pushNothing">No Push Notifications</label>
+
+
+        </fieldset>
+
+        <button className='bg-blue-500 text-white font-bold rounded py-1 px-4'>Save</button>
       </form>
     </div>
   )
